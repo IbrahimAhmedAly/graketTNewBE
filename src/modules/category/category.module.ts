0c3a325e5e9c4602cddf5a33b336854ad6f1dd48
Ibrahim Ahmed Ibrahim <ibrahim.zagglol@gmail.com>
2026-01-15 21:31:14 +0200

    Create category.module.ts

---
+++ src/modules/category/category.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { CategoryController } from './category.controller';
+import { CategoryService } from './category.service';
+
+// Repositories
+import { CategoryRepository } from './repositories/category.repository';
+
+@Module({
+  controllers: [CategoryController],
+  providers: [CategoryService, CategoryRepository],
+  exports: [CategoryService, CategoryRepository],
+})
+export class CategoryModule {}
